79574d71753a6b2d0fea2f7c8d3bcf9b6bbe7105
SyaharaniSalsabilla <syaharanibilla1@gmail.com>
2023-11-30 11:42:15 +0700

    first commit

---
+++ lib/core.dart
@@ -0,0 +1,8 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:cleaning_service/color_schema.dart';
+export 'package:cleaning_service/main_page.dart';
+export 'package:cleaning_service/splash_screen.dart';
